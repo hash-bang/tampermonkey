d5a2e3ad8844c3e7c1fcd81e915bd3be6368d30a
Matt Carter <m@ttcarter.com>
2021-09-05 15:48:54 +1000

    feat: Add Darkreader wrapper

---
+++ darkreader.user.js
@@ -0,0 +1,21 @@
+// ==UserScript==
+// @name         DarkReader
+// @namespace    https://github.com/hash-bang/tampermonkey
+// @version      0.1
+// @description  DarkReader simple wrapper without the Chrome bloat
+// @author       Matt Carter <m@ttcarter.com>
+// @match        *://*/*
+// @updateURL    https://raw.githubusercontent.com/hash-bang/tampermonkey/master/darkreader.user.js
+// @require      https://cdn.jsdelivr.net/npm/darkreader@4.9.34/darkreader.min.js
+// @run-at       document-end
+// ==/UserScript==
+
+(function() {
+	'use strict';
+
+	DarkReader.enable({
+		brightness: 100,
+		contrast: 90,
+		sepia: 10,
+	});
+})();
