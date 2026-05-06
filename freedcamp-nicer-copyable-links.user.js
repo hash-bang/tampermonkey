4b8e75fd5fe76674239b3b4c6d616996eb243489
Matt Carter <m@ttcarter.com>
2026-05-06 12:07:46 +1000

    fix: Fallback to legacy copy mode when running within Firefox

--- freedcamp-nicer-copyable-links.user.js
+++ freedcamp-nicer-copyable-links.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         FreedCamp - Nicer copyable links
 // @namespace    https://github.com/hash-bang/tampermonkey
-// @version      0.4
+// @version      0.5
 // @description  Copy full issue links in FreedCamp instead of just the title
 // @author       Matt Carter <m@ttcarter.com>
 // @match        https://freedcamp.com/*
@@ -10,6 +10,24 @@
 
 (function() {
 	'use strict';
+
+	// Firefox blocks navigator.clipboard.write() in TamperMonkey contexts; fall back to
+	// selecting a temporary off-screen element and using execCommand('copy').
+	function legacyCopy(html) {
+		var el = document.createElement('div');
+		el.innerHTML = html;
+		Object.assign(el.style, {position: 'fixed', left: '-9999px', top: '-9999px'});
+		document.body.appendChild(el);
+		var range = document.createRange();
+		range.selectNodeContents(el);
+		var sel = window.getSelection();
+		sel.removeAllRanges();
+		sel.addRange(range);
+		document.execCommand('copy');
+		sel.removeAllRanges();
+		document.body.removeChild(el);
+	}
+
 	function fcCopyReference() {
 		var issue = { // Details about this issue
 			id: $('.ItemViewFields--fk-ItemViewField-Container:last-child > .ItemViewFields--fk-ItemViewField-Value').text(),
@@ -28,12 +46,16 @@
 				+ issue.title
 			+ '</div>';
 
-		navigator.clipboard.write([
-			new ClipboardItem({
-				'text/plain': new Blob([$(html).text()], {type: 'text/plain'}), // Create plain-text version without HTML
-				'text/html': new Blob([html], {type: 'text/html'}),
-			})
-		]);
+		if (navigator.clipboard && window.ClipboardItem) {
+			navigator.clipboard.write([
+				new ClipboardItem({
+					'text/plain': new Blob([$(html).text()], {type: 'text/plain'}),
+					'text/html': new Blob([html], {type: 'text/html'}),
+				})
+			]).catch(() => legacyCopy(html));
+		} else {
+			legacyCopy(html);
+		}
 	}
 
 	$('body').on(
